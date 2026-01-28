5dcc5dcdc819c3db01b3871f0c873d3b3d64e30c
cheetahzaza555 <cheetahzaza555@gmail.com>
2026-01-28 15:44:02 +0700

    reec_1

--- back_end/routes/user.py
+++ back_end/routes/user.py
@@ -3,3 +3,11 @@ from utils.security import admin_required # เรียกยามมาใช
 from modules.database import delete_patient # สมมติว่ามีฟังก์ชันนี้
 
 user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/dashboard')
+def user_dashboard():
+    return render_template('user/index.html')
+
+@user_bp.route('/recommendations')
+def user_recommendations():
+    return render_template('user/rec_1.html')
